82b6d3a6e9a9845de8992abc96f0ccd5c2e8cc0a
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2016-04-29 12:24:30 +0300

    Remove umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,6 @@
 const webpack = require('webpack')
 
 module.exports = {
-  output: {
-    library: 'ReactPackageSkeleton',
-    libraryTarget: 'umd'
-  },
-  externals: [
-    {
-      react: {
-        root: 'React',
-        commonjs2: 'react',
-        commonjs: 'react',
-        amd: 'react'
-      }
-    }
-  ],
   module: {
     loaders: [
       {
